d76fb29ef281106d475c408aefddd1f6be24c249
JarmoTro <82982361+JarmoTro@users.noreply.github.com>
2022-06-23 15:57:22 +0300

    make 404 display correctly and minor frontend fixes

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -5,23 +5,34 @@ exports.getHomePage =  (req, res) => {
     res.render('index');
 };
 
+exports.get404Page = (req, res) => {
+    res.render('404')
+}
+
 exports.redirectUser = async (req,res) => {
     const shortURL = await URL.findOne({shortURL: req.params.shortUrl});
-    if(shortURL == null){
-        res.render('404');
+    if(shortURL != null){
+        res.redirect(shortURL.fullURL);
     }
     else{
-        res.redirect(shortURL.fullURL);
+        this.get404Page;
     }  
 };
 
 exports.createShortURL = async (req, res) => {
     if(utils.isAValidUrl(req.body.inputURL)){
-        const URL_obj = new URL({fullURL: req.body.inputURL, shortURL: utils.makeShortURLString()});
-        await URL_obj.save();
-        const shortURL = req.get('host')+"/"+URL_obj.shortURL
-        const fullURL = URL_obj.fullURL;
-        res.render('shortenedURL', {shortURL: shortURL, fullURL: fullURL});
+        while(true){
+            var shortURLString = utils.makeShortURLString();
+            var dbQuery = URL.findOne({shortURL: shortURLString});
+            if(typeof dbQuery.fullURL == 'undefined'){
+                const URL_obj = new URL({fullURL: req.body.inputURL, shortURL: shortURLString});
+                await URL_obj.save();
+                const shortURL = req.get('host')+"/"+URL_obj.shortURL
+                const fullURL = URL_obj.fullURL;
+                res.render('shortenedURL', {shortURL: shortURL, fullURL: fullURL});
+                break;
+            }
+        }
     }
     else{
         res.render('invalidURL');
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -2,10 +2,12 @@ const mainController = require('../controllers/mainController');
 const express = require('express');
 const router = express.Router();
 
+router.post('/shorten', mainController.createShortURL)
+
 router.get('/', mainController.getHomePage);
+
 router.get('/:shortUrl', mainController.redirectUser)
 
-
-router.post('/shorten', mainController.createShortURL)
+router.get('*', mainController.get404Page)
 
 module.exports = router;
